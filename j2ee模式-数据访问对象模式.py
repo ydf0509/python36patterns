69740565f63193d82008e617eccb8cf12bc082d8
FQ9H7TVDZLJ4RBT\Administrator <909686719@qq.com>
2019-10-09 17:40:22 +0800

    no message

--- j2ee模式-数据访问对象模式.py
+++ j2ee模式-数据访问对象模式.py
@@ -3,13 +3,11 @@
 # @Time    : 2019/10/9 0009 15:32
 
 """
-前端控制器模式（Front Controller Pattern）是用来提供一个集中的请求处理机制，所有的请求都将由一个单一的处理程序处理。该处理程序可以做认证/授权/记录日志，或者跟踪请求，然后把请求传给相应的处理程序。以下是这种设计模式的实体。
+数据访问对象模式（Data Access Object Pattern）或 DAO 模式用于把低级的数据访问 API 或操作从高级的业务服务中分离出来。以下是数据访问对象模式的参与者。
 
-前端控制器（Front Controller） - 处理应用程序所有类型请求的单个处理程序，应用程序可以是基于 web 的应用程序，也可以是基于桌面的应用程序。
-调度器（Dispatcher） - 前端控制器可能使用一个调度器对象来调度请求到相应的具体处理程序。
-视图（View） - 视图是为请求而创建的对象。
-
-从java转化来，命名规范懒得改了。
+数据访问对象接口（Data Access Object Interface） - 该接口定义了在一个模型对象上要执行的标准操作。
+数据访问对象实体类（Data Access Object concrete class） - 该类实现了上述的接口。该类负责从数据源获取数据，数据源可以是数据库，也可以是 xml，或者是其他的存储机制。
+模型对象/数值对象（Model Object/Value Object） - 该对象是简单的 POJO，包含了 get/set 方法来存储通过使用 DAO 类检索到的数据。
 """
 from abc import ABCMeta, abstractmethod
 from monkey_print2 import print
